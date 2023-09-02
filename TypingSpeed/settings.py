06db6bb7f39b80d6b20f4353c63b0aa3dc82f6df
Subramani <67220838+subramani2002@users.noreply.github.com>
2023-09-02 21:16:45 +0530

    Update settings.py

--- TypingSpeed/settings.py
+++ TypingSpeed/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
